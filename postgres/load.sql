4077ef9b538f2af1ee29c2fb1b9eafbf218af24d
Ziyuan (Jerry) Zhang <ziyuanzhang2019@gmail.com>
2024-03-20 13:03:18 -0400

    refactor

--- postgres/load.sql
+++ postgres/load.sql
@@ -1,95 +1,48 @@
--- Create table
-drop table if exists uoft_stations, ridership_2023, ridership_2022, station_emptiness;
-create table ridership_2023 (
-  trip_id integer primary key, 
-  trip_duration integer, 
-  start_station_id_temp text, 
-  start_time_temp text, 
-  start_station_name text, 
-  end_station_id_temp text, 
-  end_time_temp text, 
-  end_station_name text, 
-  bike_id integer, 
-  user_type text
+drop table if exists station_accessibility, station_averages;
+
+create table station_accessibility (
+    station_name varchar(100),
+    average_accessibility numeric
 );
 
-create table ridership_2022 (
-  trip_id integer primary key, 
-  trip_duration integer, 
-  start_station_id_temp text, 
-  start_time_temp text, 
-  start_station_name text, 
-  end_station_id_temp text, 
-  end_time_temp text, 
-  end_station_name text, 
-  bike_id integer, 
-  user_type text
+\copy station_accessibility from 'uoft_unavail_stations.csv' delimiter ',' csv header;
+
+update station_accessibility
+set average_accessibility = round(average_accessibility, 4);
+
+-- Create the table
+create table station_averages (
+    station_group varchar(50),
+    average_accessibility numeric
 );
 
-create table uoft_stations (
-  station_id integer primary key
-); 
+-- Insert data for stations containing specified keywords
+insert into station_averages (station_group, average_accessibility)
+select 
+    case 
+        when lower(station_name) like '%st. george%' then 'St. George'
+        when lower(station_name) like '%huron%' then 'Huron'
+        when lower(station_name) like '%bay%' then 'Bay'
+        when lower(station_name) like '%bathurst%' then 'Bathurst'
+        when lower(station_name) like '%queen''s park%' then 'Queen''s Park'
+        when lower(station_name) like '%spadina%' then 'Spadina'
+    end as station_group,
+    avg(average_accessibility) as average_accessibility
+from 
+    station_accessibility
+where 
+    lower(station_name) like any(array['%st. george%', '%huron%', '%bay%', '%bathurst%', '%queen''s park%', '%spadina%'])
+group by 
+    case 
+        when lower(station_name) like '%st. george%' then 'St. George'
+        when lower(station_name) like '%huron%' then 'Huron'
+        when lower(station_name) like '%bay%' then 'Bay'
+        when lower(station_name) like '%bathurst%' then 'Bathurst'
+        when lower(station_name) like '%queen''s park%' then 'Queen''s Park'
+        when lower(station_name) like '%spadina%' then 'Spadina'
+    end;
 
-create table station_emptiness (
-  station_id integer, 
-  time_checked timestamp,
-  num_bikes_avail integer not null, 
-  num_bikes_disabled integer not null, 
-  num_docks_avail integer not null, 
-  num_docks_disabled integer not null, 
-  station_status text not null,  
-  primary key (station_id, time_checked)
-);
 
--- trips made to and from uoft stations during morning and afternoon (rush hours) and how fall compares to winter (how much winter impacts bike usage)
--- Copy data from the CSV file into riderships, uoft_stations
-\copy ridership_2023 from '2023-01.csv' with CSV HEADER DELIMITER ',';
-\copy ridership_2023 from '2023-02.csv' with CSV HEADER DELIMITER ',';
-\copy ridership_2023 from '2023-03.csv' with CSV HEADER DELIMITER ',';
-
-\copy ridership_2022 from '2022-01.csv' with CSV HEADER DELIMITER ',';
-\copy ridership_2022 from '2022-02.csv' with CSV HEADER DELIMITER ',';
-\copy ridership_2022 from '2022-03.csv' with CSV HEADER DELIMITER ',';
-\copy ridership_2022 from '2022-04.csv' with CSV HEADER DELIMITER ',';
-\copy ridership_2022 from '2022-05.csv' with CSV HEADER DELIMITER ',';
-\copy ridership_2022 from '2022-06.csv' with CSV HEADER DELIMITER ',';
-\copy ridership_2022 from '2022-07.csv' with CSV HEADER DELIMITER ',';
-\copy ridership_2022 from '2022-08.csv' with CSV HEADER DELIMITER ',';
-\copy ridership_2022 from '2022-09.csv' with CSV HEADER DELIMITER ',';
-\copy ridership_2022 from '2022-10.csv' with CSV HEADER DELIMITER ',';
-\copy ridership_2022 from '2022-11.csv' with CSV HEADER DELIMITER ',';
-\copy ridership_2022 from '2022-12.csv' with CSV HEADER DELIMITER ',';
-
-\copy uoft_stations from 'data.csv' with CSV HEADER DELIMITER ',';
-
--- Data cleaning
-do $$ 
-declare
-  table_n text;
-begin
-  for table_n in 
-    select table_name from information_schema.tables where table_name like 'ridership%'
-  loop
-    -- Delete rows with NULL values
-    execute 'delete from ' || table_n ||
-            ' where start_station_name = ''NULL'' or end_station_name = ''NULL'' or start_station_id_temp = ''NULL'' or end_station_id_temp = ''NULL''';
-
-    -- Add columns
-    execute 'alter table ' || table_n || ' add column start_time timestamp, add column end_time timestamp, add column start_station_id integer, add column end_station_id integer';
-
-    -- Update columns
-    execute 'update ' || table_n ||
-            ' set start_time = to_timestamp(start_time_temp, ''MM/DD/YYYY HH24:MI''),' ||
-            ' end_time = to_timestamp(end_time_temp, ''MM/DD/YYYY HH24:MI''),' ||
-            ' start_station_id = cast(start_station_id_temp as integer),' ||
-            ' end_station_id = cast(end_station_id_temp as integer)';
-
-    -- Drop old columns
-    execute 'alter table ' || table_n ||
-            ' drop column start_time_temp, drop column end_time_temp,' ||
-            ' drop column start_station_id_temp, drop column end_station_id_temp';
-  end loop;
-end $$;
-
--- Display some data for verification
-select trip_id, start_time, start_station_name, end_time, end_station_name from ridership_2022 limit 20; 
+select station_group as "Street", round(average_accessibility, 4) as "Accessibility" from station_averages
+where average_accessibility > 0.1
+order by average_accessibility asc;
